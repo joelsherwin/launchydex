e0b458120a5efa139a11f0323c712bf33805acc3
Joel Sherwin <sherwinjoel@gmail.com>
2023-07-31 23:39:28 +0530

    preliminary routing commit

--- App.js
+++ App.js
@@ -3,18 +3,42 @@ import ReactDOM from "react-dom";
 
 import CardContainer from "./Components/CardContainer";
 import Header from "./Components/Header";
+import About from "./Components/About";
+import Previous from "./Components/Previous";
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const rootElem = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElem);
-
+const ErrorElement = () => {
+  return <h1>Oops, something went terribly wrong</h1>;
+};
 const App = () => {
   return (
     <>
       <Header />
-      <CardContainer />
+      <Outlet />
     </>
   );
 };
 
-root.render(<App />);
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        path: "/",
+        element: <CardContainer />,
+      },
+      {
+        path: "/previous",
+        element: <Previous />,
+      },
+    ],
+  },
+]);
+
+root.render(<RouterProvider router={routes} />);
---
+++ Components/Previous.js
@@ -0,0 +1,60 @@
+import ReactDOM from "react-dom/client";
+import React, { useEffect, useState } from "react";
+import LaunchCard from "./LaunchCard";
+import ShimmerCard from "./ShimmerCard";
+import Searchbar from "./SearchBar";
+
+import "./CardContainer.css";
+import { STALE_DATA } from "../utils/staledata.js";
+
+const Previous = () => {
+  const [launchData, setLaunchdata] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
+  const fetchLaunchData = async () => {
+    const data = await fetch(
+      "https://lldev.thespacedevs.com/2.2.0/launch/previous"
+    );
+    const JSONdata = await data.json();
+    setLaunchdata(JSONdata);
+  };
+
+  const searchInputHandler = (searchKey) => {
+    setSearchTerm(searchKey);
+  };
+
+  useEffect(() => {
+    fetchLaunchData();
+  }, []);
+
+  return launchData.length === 0 ? (
+    <div className="card-container">
+      <Searchbar keyDownHandler={searchInputHandler} />
+      <ShimmerCard />
+      <ShimmerCard />
+      <ShimmerCard />
+    </div>
+  ) : (
+    <div className="card-container">
+      <Searchbar keyDownHandler={searchInputHandler} />
+      {launchData.results
+        ?.filter((result) => {
+          if (searchTerm === "") {
+            return result;
+          } else if (
+            result.name.toLowerCase().indexOf(searchTerm.toLowerCase()) != -1 ||
+            result.pad.location.name
+              .toLowerCase()
+              .indexOf(searchTerm.toLowerCase()) != -1
+          ) {
+            return result;
+          }
+        })
+        .map((launch) => {
+          return <LaunchCard data={launch} />;
+        })}
+    </div>
+  );
+};
+
+export default Previous;
